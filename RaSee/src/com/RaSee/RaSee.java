e89469624b00384fb2a4a0cb66a0232583d08f32
hosursriharsha <hosursriharsha@gmail.com>
2019-08-16 22:27:11 +0530

    RaSee Initial Push

---
+++ RaSee/src/com/RaSee/RaSee.java
@@ -0,0 +1,62 @@
+package com.RaSee;
+
+import java.io.IOException;
+import java.util.Locale;
+import javax.speech.Central;
+import javax.speech.EngineException;
+import javax.speech.EngineStateError;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+
+public class RaSee {
+
+  /**
+   * Below method is to make RaSee speak
+   * 
+   * @param speech text to speak
+   */
+  public static void RaSeeSpeech(String speech) {
+    System.setProperty("freetts.voices",
+        "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+    try {
+      Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+      Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+      synthesizer.allocate();
+      synthesizer.resume();
+      System.out.println(speech);
+      synthesizer.speakPlainText(speech, null);
+      synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+      // synthesizer.deallocate();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException, EngineException, EngineStateError {
+    Configuration configuration = new Configuration();
+    configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+    configuration.setDictionaryPath("file:///E:\\R4U\\RaSee\\Files\\8754.dic");
+    configuration.setLanguageModelPath("file:///E:\\R4U\\RaSee\\Files\\8754.lm");
+    LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
+    recognize.startRecognition(true);
+    SpeechResult result;
+    while ((result = recognize.getResult()) != null) {
+      String command = result.getHypothesis();
+      if (command.equalsIgnoreCase("Hello")) {
+        RaSeeSpeech("Hello. RaSee here");
+      } else if (command.equalsIgnoreCase("Open notepad")) {
+        RaSeeSpeech("Can you please open ? I am busy");
+      } else if (command.equalsIgnoreCase("Howdy")) {
+        RaSeeSpeech("I am fine thank you. How are you ?");
+      } else if (command.equalsIgnoreCase("I am fine thank you")) {
+        RaSeeSpeech("What can I do for you");
+      } else if (command.equalsIgnoreCase("Bye")) {
+        RaSeeSpeech("Tata");
+        break;
+      }
+    }
+  }
+}
